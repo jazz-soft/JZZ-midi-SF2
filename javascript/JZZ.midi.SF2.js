abfe85ca93740f6d14fcd648c4e3a5869ba85e40
Sema <jazz-dev@jazz-soft.net>
2022-05-25 00:47:19 -0400

    toString()

--- javascript/JZZ.midi.SF2.js
+++ javascript/JZZ.midi.SF2.js
@@ -18,13 +18,12 @@
 
   function _error(s) { throw new Error(s); }
 
-  function _s2n(s) {
-    return s.charCodeAt(0) + 0x100 * s.charCodeAt(1) + 0x10000 * s.charCodeAt(2) + 0x1000000 * s.charCodeAt(3);
-  }
+  function _s22n(s) { return s.charCodeAt(0) + 0x100 * s.charCodeAt(1); }
+  function _s42n(s) { return s.charCodeAt(0) + 0x100 * s.charCodeAt(1) + 0x10000 * s.charCodeAt(2) + 0x1000000 * s.charCodeAt(3); }
 
   var _info = {
-    ifil: 'File version', isng: 'Target Sound Engine', INAM: 'Bank Name', irom: 'ROM Name', iver: 'ROM Version',
-    ICRD: 'Date', IENG: 'Sound Designers', IPRD: 'Product', ICOP: 'Copyright', ICMT: 'Comments', ISFT: 'Tool'
+    ifil: 'File version', isng: 'Target', INAM: 'Bank Name', irom: 'ROM Name', iver: 'ROM Version',
+    ICRD: 'Date', IENG: 'Designers', IPRD: 'Product', ICOP: 'Copyright', ICMT: 'Comments', ISFT: 'Tool'
   };
   var _pdta = {
     phdr: 'Preset Headers', pbag: 'Preset Index', pmod: 'Preset Modulators', pgen: 'Preset Generators',
@@ -78,25 +77,25 @@
     var p;
     this.data = {};
     if (s.length < 12 || s.substr(0, 4) != 'RIFF' || s.substr(8, 4) != 'sfbk') _error('Wrong file type');
-    len = _s2n(s.substr(4, 4));
+    len = _s42n(s.substr(4, 4));
     if (len != s.length - 8) _error('Corrupted file');
     p = 12;
 
     _expect(s.substr(p, 4), 'LIST');
     _expect(s.substr(p + 8, 4), 'INFO');
-    len = _s2n(s.substr(p + 4, 4));
+    len = _s42n(s.substr(p + 4, 4));
     _loadINFO(this.data, s.substr(p + 12, len - 4));
     p += len + 8;
 
     _expect(s.substr(p, 4), 'LIST');
     _expect(s.substr(p + 8, 4), 'sdta');
-    len = _s2n(s.substr(p + 4, 4));
+    len = _s42n(s.substr(p + 4, 4));
     _loadSDTA(this.data, s.substr(p + 12, len - 4));
     p += len + 8;
 
     _expect(s.substr(p, 4), 'LIST');
     _expect(s.substr(p + 8, 4), 'pdta');
-    len = _s2n(s.substr(p + 4, 4));
+    len = _s42n(s.substr(p + 4, 4));
     _loadPDTA(this.data, s.substr(p + 12, len - 4));
     p += len + 8;
   };
@@ -106,7 +105,7 @@
     var p = 0;
     var len;
     while (p < s.length) {
-      len = _s2n(s.substr(p + 4, 4));
+      len = _s42n(s.substr(p + 4, 4));
       a.push([s.substr(p, 4), s.substr(p + 8, len)]);
       p += len + 8;
     }
@@ -131,7 +130,6 @@
       d[t] = x;
     }
   }
-
   function _loadSDTA(d, s) {
     var a = _loadList(s);
     var i, t, x;
@@ -143,7 +141,6 @@
       d[t] = x;
     }
   }
-
   function _loadPDTA(d, s) {
     var a = _loadList(s);
     var i, t, x;
@@ -152,9 +149,176 @@
       x = a[i][1];
       if (d[t]) _error('Duplicate chunk: ' + t);
       if (!_pdta[t]) _error('Unexpected chunk: ' + t);
-      d[t] = x;
+      d[t] = { phdr: _phdr, pbag: _pbag, pmod: _pmod, pgen: _pgen, inst: _inst, ibag: _ibag, imod: _imod, igen: _igen, shdr: _shdr }[t](x);
     }
   }
+
+  function PHDR(a) {
+    this.name = a[0];
+    this.preset = a[1];
+    this.bank = a[2];
+    this.bag = a[3];
+    this.library = a[4];
+    this.genre = a[5];
+    this.morph = a[6];
+  }
+  PHDR.prototype.toString = function() {
+    return this.name;
+  };
+  function _phdr(s) {
+    var a = [];
+    var p = 0;
+    var n, x;
+    while (p < s.length) {
+      x = s.substr(p, 20);
+      for (n = 0; n < x.length; n++) if (!x.charCodeAt(n)) x = x.substr(0, n);
+      x = [ x ];
+      p += 20;
+      x.push(_s22n(s.substr(p, 2)));
+      p += 2;
+      x.push(_s22n(s.substr(p, 2)));
+      p += 2;
+      x.push(_s22n(s.substr(p, 2)));
+      p += 2;
+      x.push(_s42n(s.substr(p, 4)));
+      p += 4;
+      x.push(_s42n(s.substr(p, 4)));
+      p += 4;
+      x.push(_s42n(s.substr(p, 4)));
+      p += 4;
+      a.push(new PHDR(x));
+    }
+    return a;
+  }
+
+  function PBAG(a, b) {
+    this.gen = a;
+    this.mod = b;
+  }
+  PBAG.prototype.toString = function() {
+    return ['gen:', this.gen, 'mod:', this.mod].join(' ');
+  };
+  function _pbag(s) {
+    var a = [];
+    var p = 0;
+    while (p < s.length) {
+      a.push(new PBAG(_s22n(s.substr(p, 2)), _s22n(s.substr(p + 2, 4))));
+      p += 4;
+    }
+    return a;
+  }
+
+  function _pmod(s) {
+  }
+  function _pgen(s) {
+  }
+
+  function INST(a, b) {
+    this.name = a;
+    this.bag = b;
+  }
+  INST.prototype.toString = function() {
+    return this.name + ' ' + this.bag;
+  };
+  function _inst(s) {
+    var a = [];
+    var p = 0;
+    var n, x;
+    while (p < s.length) {
+      x = s.substr(p, 20);
+      for (n = 0; n < x.length; n++) if (!x.charCodeAt(n)) x = x.substr(0, n);
+      a.push(new INST(x, _s22n(s.substr(p + 20, 2))));
+      p += 22;
+    }
+    return a;
+  }
+
+  function IBAG(a, b) {
+    this.gen = a;
+    this.mod = b;
+  }
+  IBAG.prototype.toString = function() {
+    return ['gen:', this.gen, 'mod:', this.mod].join(' ');
+  };
+  function _ibag(s) {
+    var a = [];
+    var p = 0;
+    while (p < s.length) {
+      a.push(new IBAG(_s22n(s.substr(p, 2)), _s22n(s.substr(p + 2, 4))));
+      p += 4;
+    }
+    return a;
+  }
+
+  function _imod(s) {
+  }
+  function _igen(s) {
+  }
+
+
+  function SHDR(a) {
+    this.name = a[0];
+    this.start = a[1];
+    this.end = a[2];
+    this.startlp = a[3];
+    this.endlp = a[4];
+    this.rate = a[5];
+    this.key = a[6];
+    this.corr = a[7];
+    this.link = a[8];
+    this.type = a[9];
+  }
+  SHDR.prototype.toString = function() {
+    return this.name;
+  };
+  function _shdr(s) {
+    var a = [];
+    var p = 0;
+    var n, x;
+    while (p < s.length) {
+      x = s.substr(p, 20);
+      for (n = 0; n < x.length; n++) if (!x.charCodeAt(n)) x = x.substr(0, n);
+      x = [ x ];
+      p += 20;
+      x.push(_s42n(s.substr(p, 4)));
+      p += 4;
+      x.push(_s42n(s.substr(p, 4)));
+      p += 4;
+      x.push(_s42n(s.substr(p, 4)));
+      p += 4;
+      x.push(_s42n(s.substr(p, 4)));
+      p += 4;
+      x.push(_s42n(s.substr(p, 4)));
+      p += 4;
+      x.push(s.charCodeAt(p));
+      p += 1;
+      x.push(s.charCodeAt(p));
+      p += 1;
+      x.push(_s22n(s.substr(p, 2)));
+      p += 2;
+      x.push(_s22n(s.substr(p, 2)));
+      p += 2;
+      a.push(new SHDR(x));
+    }
+    return a;
+  }
+
+  function _n2v(a) { return a ? a[0] + ('.0' + a[1]).substr(0, 3) : ''; }
+  var _info_tags = ['isng', 'INAM', 'irom', 'iver', 'ICRD', 'IENG', 'IPRD', 'ICOP', 'ICMT', 'ISFT'];
+  var _pdta_tags = ['phdr', 'pbag',   'inst', 'ibag', 'shdr'];
+
+  SF2.prototype.toString = function() {
+    var i, j, x;
+    var a = ['SOUNDFONT ' + _n2v(this.data.ifil)];
+    for (i = 0; i < _info_tags.length; i++) if (this.data[_info_tags[i]]) a.push('  ' + (_info[_info_tags[i]] + ':        ').substr(0, 13) + this.data[_info_tags[i]]);
+    a.push('  Sample data: [ ' + this.data.smpl.length + ' ]');
+    for (i = 0; i < _pdta_tags.length; i++) {
+      x = this.data[_pdta_tags[i]];
+      a.push('  ' + (_pdta[_pdta_tags[i]] + ':        ').substr(0, 15) + '[ ' + x.length + ' ]');
+      for (j = 0; j < x.length; j++) a.push('    ' + x[j]);
+    }
+    return a.join('\n');
+  };
 
   JZZ.MIDI.SF2 = SF2;
 });
--- test.js
+++ test.js
@@ -5,3 +5,4 @@ var file = process.argv[2];
 if (typeof file == 'undefined') file = 'test.sf2';
 var data = require('fs').readFileSync(file, 'binary');
 var sf = new JZZ.MIDI.SF2(data);
+console.log(sf.toString());
