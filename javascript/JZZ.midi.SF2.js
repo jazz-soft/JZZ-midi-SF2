99ad405d84d82e5c20f959b8c96ea7323eacbb5a
Sema <jazz-dev@jazz-soft.net>
2022-05-30 01:15:38 -0400

    Handle in-browser errors

--- javascript/JZZ.midi.SF2.js
+++ javascript/JZZ.midi.SF2.js
@@ -38,32 +38,31 @@
       if (arguments[0] instanceof SF2) {
         return arguments[0].copy();
       }
+      var data;
       try {
         if (arguments[0] instanceof ArrayBuffer) {
-          self.load(String.fromCharCode.apply(null, new Uint8Array(arguments[0])));
-          return self;
+          data = String.fromCharCode.apply(null, new Uint8Array(arguments[0]));
         }
       }
       catch (err) {/**/}
       try {
         if (arguments[0] instanceof Uint8Array || arguments[0] instanceof Int8Array) {
-          self.load(String.fromCharCode.apply(null, new Uint8Array(arguments[0])));
-          return self;
+          data = String.fromCharCode.apply(null, new Uint8Array(arguments[0]));
         }
       }
       catch (err) {/**/}
       try {
         /* istanbul ignore next */
         if (arguments[0] instanceof Buffer) {
-          self.load(arguments[0].toString('binary'));
-          return self;
+          data = arguments[0].toString('binary');
         }
       }
       catch (err) {/**/}
-      if (typeof arguments[0] == 'string' && arguments[0] != '0' && arguments[0] != '1' && arguments[0] != '2') {
-        self.load(arguments[0]);
-        return self;
+      if (typeof arguments[0] == 'string') {
+        data = arguments[0];
       }
+      self.load(data);
+      return self;
     }
   }
   SF2.version = function() { return _ver; };
@@ -76,7 +75,7 @@
     var len;
     var p;
     this.data = {};
-    if (s.length < 12 || s.substr(0, 4) != 'RIFF' || s.substr(8, 4) != 'sfbk') _error('Wrong file type');
+    if (s.length < 12 || s.substr(0, 4) != 'RIFF' || s.substr(8, 4) != 'sfbk') _error('Not a SoundFont file');
     len = _s42n(s.substr(4, 4));
     if (len != s.length - 8) _error('Corrupted file');
     p = 12;
